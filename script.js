d44481c019573525e92352eda2a40b7fec6f6120
fourwakir-a11y <fourwakir@gmail.com>
2026-06-15 12:46:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const games = [
+{
+ title: "Snake",
+ banner: "snake-banner.jpg",
+ description: "Classic snake."
+},
+{
+ title: "Flappy Bird",
+ banner: "flappy-banner.jpg",
+ description: "Fly through pipes."
+}
+];
+heroTitle.textContent = game.title;
+heroDesc.textContent = game.description;
+heroImage.src = game.banner;
