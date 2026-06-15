e8cbcd81cec47105bf27e29cd365bd583261ed38
fourwakir-a11y <fourwakir@gmail.com>
2026-06-15 13:53:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ function changeGame(title, desc, image) {
   document.getElementById("title").innerText = title;
   document.getElementById("description").innerText = desc;
 
-  hero.style.opacity = "0.4";
-
-  setTimeout(() => {
-    hero.style.backgroundImage =
-      `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.9)), url(${image})`;
-
-    hero.style.opacity = "1";
-  }, 120);
+  hero.style.backgroundImage =
+    `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.9)), url(${image})`;
 }
